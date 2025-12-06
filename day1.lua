3378497de31af76630c284ac7ce99911d603b1f3
Eduardo Barroso <edbar42@proton.me>
2025-12-05 21:36:47 -0300

    solved day 1

--- day1.lua
+++ day1.lua
@@ -1,16 +1,23 @@
-function read_solution_file()
-	local f = io.open("input.txt", "rb")
-	if f == nil then
-		print("could not read input.txt")
-		return
-	end
-
-	for l in f:lines() do
-		print(l)
-	end
-
-	f.close(f)
+local dial = 50
+local passwd = 0
+local f = io.open("input.txt", "r")
+if f == nil then
+	print("could not read input.txt")
+	return
 end
 
--- local start = 50
-data = read_solution_file()
+for l in f:lines() do
+	local turn_str, many = l:match("^([ LR ])(%d+)")
+	if turn_str == "L" then
+		dial = dial - many
+	elseif turn_str == "R" then
+		dial = dial + many
+	end
+	if dial % 100 == 0 then
+		passwd = passwd + 1
+	end
+end
+
+print(passwd)
+
+f.close(f)
